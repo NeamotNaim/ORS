1bbd004b47839583febecaf568fae26bae29c51b
NeamotNaim <neamotnaim123@gmail.com>
2023-01-11 21:14:02 +0600

    laravel spatie permisssion seeder added

---
+++ database/seeders/PermissionsDemoSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\PermissionRegistrar;
+use Illuminate\Support\Facades\Hash;
+
+class PermissionsDemoSeeder extends Seeder
+{
+    /**
+     * Create the initial roles and permissions.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Reset cached roles and permissions
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // create permissions
+        Permission::create(['name' => 'edit articles']);
+        Permission::create(['name' => 'delete articles']);
+        Permission::create(['name' => 'publish articles']);
+        Permission::create(['name' => 'unpublish articles']);
+        Permission::create(['name' => 'update articles']);
+
+        // create roles and assign existing permissions
+        $role1 = Role::create(['name' => 'writer']);
+        $role1->givePermissionTo('edit articles');
+        $role1->givePermissionTo('delete articles');
+
+        $role2 = Role::create(['name' => 'admin']);
+        $role2->givePermissionTo('publish articles');
+        $role2->givePermissionTo('unpublish articles');
+
+        $role3 = Role::create(['name' => 'Super-Admin']);
+        // gets all permissions via Gate::before rule; see AuthServiceProvider
+
+        // create demo users
+        $user = \App\Models\User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'password' => Hash::make('12345678'), // password
+        ]);
+        $user->assignRole($role1);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('12345678'), // password
+        ]);
+        $user->assignRole($role2);
+
+        $user = \App\Models\User::factory()->create([
+            'name' => 'Super-Admin',
+            'email' => 'superadmin@gmail.com',
+            'password' => Hash::make('12345678'), // password
+        ]);
+        $user->assignRole($role3);
+    }
+}
